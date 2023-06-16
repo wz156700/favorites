a7f7adbd29e85d22a1e071485ddabb0c688c7792
wanglf <3066966420@qq.com>
2023-06-16 19:05:21 +0800

    实现图片预览

--- readit/src/main.ts
+++ readit/src/main.ts
@@ -4,6 +4,7 @@ import 'normalize.css' // 统一浏览器差异
 import '@/assets/common.css'
 import { createPinia } from 'pinia'
 import router from '@/router/index.js'
+//引入图片预览
+import preview from 'vue3-image-preview';
 
-
-createApp(App).use(createPinia()).use(router).mount('#app')
+createApp(App).use(createPinia()).use(router).use(preview).mount('#app')
--- readit/src/views/ImageGallery.vue
+++ readit/src/views/ImageGallery.vue
@@ -1,34 +1,125 @@
 <script setup>
 import Header from "@/views/header.vue";
 import { onMounted, ref } from "vue";
+import { preview, vPreview, Vue3ImagePreview } from "vue3-image-preview";
+// const { proxy } = getCurrentInstance();
+
+// proxy.preview({
+//   images: state.images1,
+//   index: state.index,
+// });
 
 let fileList = ref([]);
 
-const cb = async () => {
+// 获取图片列表
+const getImgList = async () => {
   let file = await myApi.getFileList();
   fileList.value = file;
 };
 
+//接收到主进程返回消息之后的回调函数
+const cb = async () => {
+  getImgList();
+};
+
+const previewImg = (item) => {
+  console.log();
+  preview({ images: `${window.location.href.split("#")[0]}/upload/${item}` });
+};
+
 onMounted(async () => {
   myApi.getFileListOnMain(cb);
-  let file = await myApi.getFileList();
-  fileList.value = file;
+  getImgList();
+  console.log(fileList);
 });
 </script>
 
 <template>
   <Header />
   <div class="image-container">
-    <img :src="`/upload/${item}`" alt="" v-for="item in fileList" :key="item" />
+    <div class="img-contain" v-for="item in fileList" :key="item">
+      <img v-preview :src="`/upload/${item}`" alt="" />
+      <div class="mask">
+        <div class="icon-contain">
+          <svg
+            t="1686910698530"
+            class="preview"
+            viewBox="0 0 1024 1024"
+            version="1.1"
+            xmlns="http://www.w3.org/2000/svg"
+            p-id="2690"
+            width="30"
+            height="30"
+            @click="previewImg(item)"
+          >
+            <path
+              d="M512 896C271.232 896 32 686.016 32 512S271.232 128 512 128c240.96 0 480 209.6 480 384s-239.04 384-480 384z m0-64c207.36 0 416-182.976 416-320S719.36 192 512 192C304.896 192 96 375.296 96 512s208.896 320 416 320z m0-128a192 192 0 1 1 0-384 192 192 0 0 1 0 384z m0-64a128 128 0 1 0 0-256 128 128 0 0 0 0 256z"
+              fill="#ffffff"
+              p-id="2691"
+            ></path>
+          </svg>
+          <svg
+            t="1686910730141"
+            class="delete"
+            viewBox="0 0 1024 1024"
+            version="1.1"
+            xmlns="http://www.w3.org/2000/svg"
+            p-id="3751"
+            width="30"
+            height="30"
+          >
+            <path
+              d="M256 256h554.666667v640H256V256z m42.666667 42.666667v554.666666h469.333333V298.666667H298.666667z m128 128h42.666666v298.666666h-42.666666v-298.666666z m170.666666 0h42.666667v298.666666h-42.666667v-298.666666zM213.333333 256h640v42.666667H213.333333V256z m213.333334-85.333333h213.333333v42.666666h-213.333333V170.666667z"
+              fill="#ffffff"
+              p-id="3752"
+            ></path>
+          </svg>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <style scoped lang='stylus'>
 .image-container {
+  display: flex;
+  flex-wrap: wrap;
   padding: 10px;
 
-  img {
+  .img-contain {
+    position: relative;
     width: 200px;
-    margin-right: 10px;
+    margin: 10px;
+    cursor: pointer;
+
+    img {
+      width: 100%;
+    }
+
+    .mask {
+      display: flex;
+      align-items: center;
+      position: absolute;
+      left: 0;
+      top: 0;
+      width: 100%;
+      height: 100%;
+      background: rgba(0, 0, 0, 0.5);
+      z-index: 999;
+      opacity: 0;
+
+      .icon-contain {
+        width: 100%;
+        text-align: center;
+
+        svg {
+          margin: 20px;
+        }
+      }
+    }
+  }
+
+  .img-contain:hover .mask {
+    opacity: 1;
   }
 }
 </style>
